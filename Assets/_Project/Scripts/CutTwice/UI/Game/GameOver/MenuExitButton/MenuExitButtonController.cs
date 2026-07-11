66980b09ab2a966953ece5667ad4bb9df9052d2c
Yan Vishnevskiy <64552288+emmersmacks@users.noreply.github.com>
2026-07-11 23:01:30 +0500

    fix menu edit exit button controller logic

--- Assets/_Project/Scripts/CutTwice/UI/Game/GameOver/MenuExitButton/MenuExitButtonController.cs
+++ Assets/_Project/Scripts/CutTwice/UI/Game/GameOver/MenuExitButton/MenuExitButtonController.cs
@@ -1,19 +1,33 @@
-﻿using CutTwice.Core.RivletUI;
-using CutTwice.Core.StaticNames;
-using UnityEngine.SceneManagement;
+﻿using System.Threading;
+using CutTwice.Core.GameStates;
+using CutTwice.Core.Lifecycle;
+using CutTwice.Core.RivletUI;
+using CutTwice.Menu.GlobalStates;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace CutTwice.UI.Game.GameOver.MenuExitButton
 {
-    public class MenuExitButtonController : WindowControllerBase<MenuExitButtonView>
+    public class MenuExitButtonController : WindowControllerBase<MenuExitButtonView>, IInitializable
     {
-        public MenuExitButtonController(MenuExitButtonView view) : base(view)
+        private readonly GlobalStateMachine _globalStateMachine;
+        private CancellationToken _cancellationToken;
+
+        public MenuExitButtonController(MenuExitButtonView view, GlobalStateMachine globalStateMachine) : base(view)
         {
+            _globalStateMachine = globalStateMachine;
             view.Button.onClick.AddListener(ExitMenu);
+        }
+
+        public UniTask InitAsync(CancellationToken ct)
+        {
+            _cancellationToken = ct;
+            return UniTask.CompletedTask;
         }
 
         private void ExitMenu()
         {
-            SceneManager.LoadScene(SceneNames.MainMenu);
+            _globalStateMachine.SetStateAsync<GlobalMainMenuState>(_cancellationToken).Forget(Debug.LogException);
         }
     }
 }
